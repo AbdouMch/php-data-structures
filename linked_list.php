33c782a6fe1dbfc3c2a4a17461fcc260b904e954
Abderrahmen MCHAYAA <abderrahmen.mchaaya@talan.com>
2024-04-17 00:37:56 +0100

    Linked list search, insert and delete

--- linked_list.php
+++ linked_list.php
@@ -52,6 +52,145 @@ class LinkedList
     }
 
     /**
+     * returns the first occurrence of a value in the list
+     * returns null if the value not found
+     * takes O(n) time
+     */
+    public function search(int $value): ?Node
+    {
+        $current = $this->head;
+
+        while (null !== $current) {
+            if ($value === $current->data) {
+                return  $current;
+            }
+            $current = $current->nextNode;
+        }
+
+        return null;
+    }
+
+    /**
+     * Inserts a new node at index position
+     * Insertion takes O(1) time but finding the position is O(n) time
+     * So the insertion method takes O(n) time
+     */
+    public function insert(int $data, int $index): self
+    {
+        if (0 === $index) {
+            $this->add($data);
+            return $this;
+        }
+
+        $position = $index;
+        $current = $this->head;
+
+        while ($position > 1 && null !== $current) {
+            $current = $current->nextNode;
+            $position--;
+        }
+
+        if (null === $current) {
+            throw new Exception("out of range index!");
+        }
+
+        $new = new Node($data);
+        $previous = $current;
+        $next = $current->nextNode;
+        $previous->nextNode = $new;
+        $new->nextNode = $next;
+
+        return $this;
+    }
+
+    /**
+     * Removes a node by value
+     * Returns the node or null if not found
+     * Takes O(n) time
+     */
+    public function removeByIndex(int $index): ?Node
+    {
+        if (0 === $index) {
+            $removed = $this->head;
+            $this->head = $this->head->nextNode;
+
+            return $removed;
+        }
+
+        $position = $index;
+        $current = $this->head;
+
+        while ($position > 1 && null !== $current) {
+            $current = $current->nextNode;
+            $position--;
+        }
+
+        if (null === $current) {
+            throw new Exception("out of range index!");
+        }
+
+        $previous = $current;
+        $toRemove = $current->nextNode;
+        $next = $toRemove->nextNode;
+        $previous->nextNode = $next;
+
+        return $toRemove;
+    }
+
+    /**
+     * Removes a node by value
+     * Returns the node or null if not found
+     * Takes O(n) time
+     */
+    public function remove(int $data): ?Node
+    {
+        $previous = null;
+        $current = $this->head;
+        $found = false;
+
+        while (null !== $current && false === $found) {
+            if ($data === $current->data && $current === $this->head) {
+                $this->head = $current->nextNode;
+                $found = true;
+            } elseif ($data === $current->data) {
+                $previous->nextNode = $current->nextNode;
+                $found = true;
+            }
+
+            $previous = $current;
+            $current = $current->nextNode;
+        }
+
+        if ($found) {
+            return $current;
+        }
+
+        return null;
+    }
+
+    public function removeAll(int $data): self
+    {
+        $previous = null;
+        $current = $this->head;
+
+        while (null !== $current) {
+            if ($data === $current->data) {
+                $next = $current->nextNode;
+                if (null !== $previous) {
+                    $previous->nextNode = $next;
+                } else {
+                    $this->head = $next;
+                }
+            } else {
+                $previous = $current;
+            }
+            $current = $current->nextNode;
+        }
+
+        return $this;
+    }
+
+    /**
      * Returns the size of nodes in the list takes O(n) time (linear time)
      */
     public function size(): int
@@ -69,6 +208,10 @@ class LinkedList
 
     public function __toString()
     {
+        if (null === $this->head) {
+            return 'empty list';
+        }
+
         $rep = [];
 
         $current = $this->head;
@@ -122,5 +265,85 @@ $linkedList
     ->add(30)
     ;
 echo "size is " . $linkedList->size() . PHP_EOL;
+
+echo $linkedList . PHP_EOL;
+
+echo "search 12 : " . (null !== $linkedList->search(12) ? 'found' : 'not found') . PHP_EOL;
+echo "search 300 : " . (null !== $linkedList->search(300) ? 'found' : 'not found') . PHP_EOL;
+
+$linkedList = new LinkedList();
+
+$linkedList
+    ->add(30)
+    ->add(20)
+    ->add(10)
+    ;
+
+echo $linkedList . PHP_EOL;
+
+$linkedList->insert(15, 1);
+
+echo $linkedList . PHP_EOL;
+
+$linkedList->insert(5, 0);
+
+echo $linkedList . PHP_EOL;
+
+$linkedList->insert(40, 5);
+
+echo $linkedList . PHP_EOL;
+
+$removed = $linkedList->removeByIndex(0);
+
+echo "removed node " . $removed . " at index 0". PHP_EOL;
+echo $linkedList . PHP_EOL;
+
+$removed = $linkedList->removeByIndex(4);
+
+echo "removed node " . $removed . " at index 4". PHP_EOL;
+echo $linkedList . PHP_EOL;
+
+$linkedList->removeByIndex(1);
+
+echo $linkedList . PHP_EOL;
+
+
+echo "Remove By value" . PHP_EOL;
+
+
+$linkedList = new LinkedList();
+
+$linkedList
+    ->add(30)
+    ->add(30)
+    ->add(20)
+    ->add(5)
+    ->add(20)
+    ->add(10)
+    ->add(10)
+;
+
+echo $linkedList . PHP_EOL;
+
+$linkedList->remove(30);
+
+
+echo $linkedList . PHP_EOL;
+
+$linkedList->removeAll(30);
+
+
+echo $linkedList . PHP_EOL;
+
+$linkedList->removeAll(10);
+
+echo $linkedList . PHP_EOL;
+
+$linkedList->removeAll(20);
+
+echo $linkedList . PHP_EOL;
+
+
+$linkedList->removeAll(5);
 
 echo $linkedList . PHP_EOL;
